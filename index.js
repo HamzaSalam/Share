9c514f23606c30597389a2c82b11fb31e54011eb
hamza salam <hamza.salam786@gmail.com>
2024-12-12 13:16:14 +0500

    project setup

--- index.js
+++ index.js
@@ -2,8 +2,23 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Text, TextInput} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+//hum user ko apna hi fontsize or fontfamliy dekhana chaty he user agr change bi krta ha to hum usko esk code k zariy allow ni kren gen k wo change kry
+
+if (Text.defaultProps) {
+  Text.defaultProps.allowFontScaling = false;
+} else {
+  Text.defaultProps = {};
+  Text.defaultProps.allowFontScaling = false;
+}
+
+if (TextInput.defaultProps) {
+  TextInput.defaultProps.allowFontScaling = false;
+} else {
+  TextInput.defaultProps = {};
+  TextInput.defaultProps.allowFontScaling = false;
+}
 AppRegistry.registerComponent(appName, () => App);
